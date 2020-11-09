50e3c0251a11b8add1c1f7b612fca298fc46a93e
al385729 <al385729@uji.es>
2020-11-09 14:24:37 +0100

    pipes scripts and puzle manager

---
+++ Assets/Scripts/Puzzles/pipesPuzle/PipeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeScript : MonoBehaviour
+{
+    float[] rotations = {0, 90, 180, 270 };
+    public float correctRotation;
+    public bool works;
+
+    [SerializeField]
+    bool isPlaced = false;
+
+    private void Start()
+    {
+        correctRotation = transform.rotation.z;
+        int rand = Random.Range(0, rotations.Length);
+        transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
+
+        if (transform.eulerAngles.z == correctRotation && works)
+        {
+            isPlaced = true;
+        }
+    }
+    private void OnMouseDown()
+    {
+        transform.Rotate(new Vector3(0, 0, 90));
+
+        if(transform.eulerAngles.z == correctRotation && !isPlaced && works)
+        {
+            isPlaced = true;
+        }
+        else if (isPlaced)
+        {
+            isPlaced = false;
+        }
+    }
+}
