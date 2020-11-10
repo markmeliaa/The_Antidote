371d1a6792bc8a89accafe2a299cd650428c4812
al385729 <al385729@uji.es>
2020-11-10 13:27:21 +0100

    Pipes building

--- Assets/Scripts/Puzzles/pipesPuzle/PipeScript.cs
+++ Assets/Scripts/Puzzles/pipesPuzle/PipeScript.cs
@@ -4,35 +4,63 @@ using UnityEngine;
 
 public class PipeScript : MonoBehaviour
 {
-    float[] rotations = {0, 90, 180, 270 };
-    public float correctRotation;
-    public bool works;
+    public float[] correctRotation;
+    public int[] connectedPipes;
 
     [SerializeField]
     bool isPlaced = false;
 
+    int posibleRotation = 1;
+    PuzleManager puzleManager;
+    float[] rotations = { 0, 90, 180, 270 };
+
+    private void Awake()
+    {
+        puzleManager = GameObject.Find("PuzleManager").GetComponent<PuzleManager>();
+    }
+
     private void Start()
     {
-        correctRotation = transform.rotation.z;
+        posibleRotation = correctRotation.Length;
+        correctRotation[0] = transform.rotation.z;
+        if(posibleRotation == 2)
+        {
+            correctRotation[1] = 180;
+        }
+        else
+        {
+            for (int i = 0; i < posibleRotation; i++)
+            {
+                correctRotation[i] = 90 * i;
+            }
+        }
+
         int rand = Random.Range(0, rotations.Length);
         transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
 
-        if (transform.eulerAngles.z == correctRotation && works)
+        for (int i = 0; i < posibleRotation; i++)
         {
-            isPlaced = true;
+            if (transform.eulerAngles.z > correctRotation[i] - 1 && transform.eulerAngles.z < correctRotation[i] + 1)
+            {
+                isPlaced = true;
+            }
         }
     }
     private void OnMouseDown()
     {
         transform.Rotate(new Vector3(0, 0, 90));
 
-        if(transform.eulerAngles.z == correctRotation && !isPlaced && works)
+        for (int i = 0; i < posibleRotation; i++)
         {
-            isPlaced = true;
-        }
-        else if (isPlaced)
-        {
-            isPlaced = false;
+            if (transform.eulerAngles.z > correctRotation[i] - 1 && transform.eulerAngles.z < correctRotation[i] + 1)
+            {
+                isPlaced = true;
+                break;
+            }
+            else
+            {
+                isPlaced = false;
+            }
         }
     }
 }
--- Assets/Scripts/Puzzles/pipesPuzle/PuzleManager.cs
+++ Assets/Scripts/Puzzles/pipesPuzle/PuzleManager.cs
@@ -15,6 +15,7 @@ public class PuzleManager : MonoBehaviour
         totalPipes = PipesHolder.transform.childCount;
         Pipes = new GameObject[totalPipes];
 
+        int k = 0;
         for (int i = 0; i < Pipes.Length; i++)
         {
             Pipes[i] = PipesHolder.transform.GetChild(i).gameObject.transform.GetChild(0).gameObject;
