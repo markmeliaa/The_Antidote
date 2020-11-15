04763083fdab22b4e2debe22a7688cac6527d3c8
al385729 <al385729@uji.es>
2020-11-15 15:43:37 +0100

    otro commit por bug

---
+++ Assets/Scripts/Puzzles/pipesPuzle/PipeScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PipeScript : MonoBehaviour
+{
+    public sealed class Pair
+    {
+        public int i;
+        public int j;
+        public Pair(int newi, int newj)
+        {
+            i = newi;
+            j = newj;
+        }
+    };
+
+    public List<Pair> connectedPipes;
+    public int myI = 0;
+    public int myJ = 0;
+    public int types; //type 0 start, type 1 end, type 2 L, type 3 horizontal, type 4 +
+
+
+    public Sprite waterSprite;
+    public Sprite pipeSprite;
+    public SpriteRenderer sp;
+    public PuzleManager puzleManager;
+
+    public bool notRotable;
+    public bool connected = false;
+
+    float[] rotations = { 0, 90, 180, 270 };
+    
+
+    private void Awake()
+    {
+        connectedPipes = new List<Pair>();
+        sp = GetComponent<SpriteRenderer>();
+        puzleManager = GameObject.Find("PuzleManager").GetComponent<PuzleManager>();
+    }
+
+    private void Start()
+    {
+        if (!notRotable)
+        {
+            int rand = Random.Range(0, rotations.Length);
+            transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
+        }
+
+        if (types != 0)
+        {
+            for (int i = 0; i < connectedPipes.Count; i++)
+            {
+                if (types == 1)
+                    notRotable = true;
+                else
+                    notRotable = false;
+
+                checkPipes(i);
+            }
+        }
+        else
+        {
+            connected = true;
+            notRotable = true;
+        }  
+    }
+
+    private void Update()
+    {
+        if(types != 0)
+        {
+            int count = 0;
+            for (int i = 0; i < connectedPipes.Count; i++)
+            {
+                count += checkPipes(i);
+            }
+            if (count == connectedPipes.Count)
+                connected = false;
+        }
+
+        if (connected)
+            sp.sprite = waterSprite;
+        else
+            sp.sprite = pipeSprite;
+    }
+    private void OnMouseDown()
+    {
+        if (!notRotable)
+        {
+            transform.Rotate(new Vector3(0, 0, 90));
+        }
+        connected = false;
+    }
+
+    public abstract int checkPipes(int indice);
+}
---
+++ Assets/Scripts/Puzzles/pipesPuzle/PuzleManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzleManager : MonoBehaviour
+{
+    public GameObject PipesHolder;
+    public GameObject[,] Pipes;
+    public Text endText;
+
+    GameObject lastPipe;
+
+    [SerializeField]
+    int totalPipes = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalPipes = PipesHolder.transform.childCount;
+        Pipes = new GameObject[totalPipes/5, 5];
+
+        int col = 0;
+        int fil = 0;
+        for (int i = 0; i < PipesHolder.transform.childCount; i++)
+        {
+            Pipes[fil, col] = PipesHolder.transform.GetChild(i).gameObject.transform.GetChild(0).gameObject;
+
+            if(Pipes[fil, col].GetComponent<PipeScript>().types == 1)
+            {
+                lastPipe = Pipes[fil, col];
+            }
+
+            col++;
+            if (col >= 5)
+            {
+                col = 0;
+                fil++;
+            }   
+        }
+
+        for (int i = 0; i < Pipes.GetLength(0); i++)
+        {
+            for (int j = 0; j < Pipes.GetLength(1); j++)
+            {
+                PipeScript currentPipe = Pipes[i, j].GetComponent<PipeScript>();
+
+                currentPipe.myI = i;
+                currentPipe.myJ = j;
+
+                if (i != 0)
+                    currentPipe.connectedPipes.Add(new PipeScript.Pair(i - 1, j));
+
+                if (i != (Pipes.GetLength(0) - 1))
+                    currentPipe.connectedPipes.Add(new PipeScript.Pair(i + 1, j));
+
+                if (j != 0)
+                    currentPipe.connectedPipes.Add(new PipeScript.Pair(i, j - 1));
+
+                if (j != (Pipes.GetLength(1) - 1))
+                    currentPipe.connectedPipes.Add(new PipeScript.Pair(i, j + 1));
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (lastPipe.GetComponent<PipeScript>().connected)
+            endPuzle();
+    }
+
+    private void endPuzle()
+    {
+        endText.gameObject.SetActive(true);
+        for (int i = 0; i < Pipes.GetLength(0); i++)
+        {
+            for (int j = 0; j < Pipes.GetLength(1); j++)
+            {
+                Pipes[i, j].GetComponent<PipeScript>().notRotable = true;
+            }
+        }
+    }
+}
