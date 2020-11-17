462561f67ad5ae22656b2779e390695f40670a64
Mark Meliaa <marcalconmelia@gmail.com>
2020-11-17 22:08:41 +0100

    Moving sprites

---
+++ Assets/Scripts/Puzzles/shadowPuzle/MoveSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSystem : MonoBehaviour
+{
+    public GameObject correctForm;
+    private bool moving;
+
+    private float startPosX;
+    private float startPosY;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (moving == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, gameObject.transform.localPosition.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosY = mousePos.x - this.transform.localPosition.x;
+            startPosX = mousePos.y - this.transform.localPosition.y;
+
+            moving = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        moving = false;
+    }
+}
