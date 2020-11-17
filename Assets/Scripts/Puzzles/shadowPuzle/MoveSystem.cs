30e9eb4ac28d06945259a0fab105ef386b9b0843
Mark Meliaa <marcalconmelia@gmail.com>
2020-11-17 22:24:04 +0100

    Add colision and finish puzzle

--- Assets/Scripts/Puzzles/shadowPuzle/MoveSystem.cs
+++ Assets/Scripts/Puzzles/shadowPuzle/MoveSystem.cs
@@ -6,24 +6,30 @@ public class MoveSystem : MonoBehaviour
 {
     public GameObject correctForm;
     private bool moving;
+    private bool finish;
 
     private float startPosX;
     private float startPosY;
 
+    private Vector3 resetPosition;
+
     void Start()
     {
-        
+        resetPosition = this.transform.position;
     }
 
     void Update()
     {
-        if (moving == true)
+        if (!finish)
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            if (moving)
+            {
+                Vector3 mousePos;
+                mousePos = Input.mousePosition;
+                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, gameObject.transform.localPosition.z);
+                this.gameObject.transform.position = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, gameObject.transform.position.z);
+            }
         }
     }
 
@@ -35,8 +41,8 @@ public class MoveSystem : MonoBehaviour
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-            startPosY = mousePos.x - this.transform.localPosition.x;
-            startPosX = mousePos.y - this.transform.localPosition.y;
+            startPosY = mousePos.x - this.transform.position.x;
+            startPosX = mousePos.y - this.transform.position.y;
 
             moving = true;
         }
@@ -45,5 +51,18 @@ public class MoveSystem : MonoBehaviour
     private void OnMouseUp()
     {
         moving = false;
+
+        if ((Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f) && (Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f))
+        {
+            this.transform.localPosition = new Vector3(correctForm.transform.localPosition.x, correctForm.transform.localPosition.y, correctForm.transform.localPosition.z);
+            finish = true;
+
+            GameObject.Find("PointHandler").GetComponent<WinScript>().AddPoints();
+        }
+
+        else
+        {
+            this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
+        }
     }
 }
