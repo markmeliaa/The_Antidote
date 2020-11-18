cccbeb6a2b45345794000e0683f3bfcd3f9bd1ff
al385729 <al385729@uji.es>
2020-11-18 22:15:09 +0100

    kierans house dialogs

--- Assets/Scripts/Dialogue/Tester.cs
+++ Assets/Scripts/Dialogue/Tester.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class Tester : MonoBehaviour
 {
-    public Conversation convo;
     public GameObject convoButton;
+    [SerializeField] private Conversation[] convo;
+    List<bool> activate;
+    int index = 0;
 
+    public void Start()
+    {
+        activate = new List<bool>();
+
+        activate.Add(true);
+        for (int i = 1; i < convo.Length; i++)
+        {
+            activate.Add(false);
+        }
+    }
     public void starConvo()
     {
-        DialogueManager.StartConversation(convo);
+        if (activate[index])
+        {
+            DialogueManager.StartConversation(convo[index]);
+
+            index++;
+            if (index < convo.Length)
+                activate[index] = true;
+            else
+                index = 0;
+        }
     }
 
     public void destroyButton()
---
+++ Assets/Scripts/Dialogue/TestingDialogs/AutomaticDialogs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticDialogs : MonoBehaviour
+{
+    public float finalTime;
+
+    float timer;
+    bool activated = false;
+
+    [SerializeField]
+    Tester tester;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tester = GetComponent<Tester>();
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!activated)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= finalTime)
+            {
+                timer = 0;
+                activated = true;
+                tester.starConvo();
+            }
+        }
+    }
+}
