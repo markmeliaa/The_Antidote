e40e0d6d097019dc718de740f937fd372890a12a
al385729 <al385729@uji.es>
2020-11-22 11:39:06 +0100

    background changing

---
+++ Assets/Scripts/Background Changes/backgoundChange.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgoundChange : MonoBehaviour
+{
+    public Sprite newBackground;
+    public GameObject miniMap;
+
+    [SerializeField] bool change = false;
+    float timer = 0.0f;
+    float time = 2.0f;
+
+    private void Update()
+    {
+        if (change)
+        {
+            if(timer == 0.0f)
+            {
+                for(int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
+
+                miniMap.SetActive(false);
+            }
+
+            timer += Time.deltaTime;
+
+            if(timer >= time)
+            {
+                timer = 0;
+                change = false;
+
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+
+                miniMap.SetActive(true);
+                transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = newBackground;
+            }
+
+        }
+    }
+
+    public void changeBackground()
+    {
+        change = true;
+    }
+}
