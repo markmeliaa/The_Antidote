c41c88c15a913061a577406c6c006bd07d61ff5d
Mark Meliaa <marcalconmelia@gmail.com>
2020-11-23 23:39:10 +0100

    Added change between scenes

---
+++ Assets/Scripts/ChangeScene/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public int nextScene;
+
+    private void OnMouseDown()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != nextScene)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+       StartCoroutine(LoadLevel(nextScene));
+    }
+
+    IEnumerator LoadLevel(int nextScene)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(nextScene);
+    }
+}
