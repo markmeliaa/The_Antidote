5e66560bbd666d0354893e63cb537e44b8602ed8
Mark Meliaa <marcalconmelia@gmail.com>
2020-11-28 21:20:59 +0100

    Cells created

--- Assets/Scripts/Puzzles/mazePuzle/Maze.cs
+++ Assets/Scripts/Puzzles/mazePuzle/Maze.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Maze : MonoBehaviour
 {
+    [System.Serializable]
+    public class Cell
+    {
+        public bool visited;
+        public GameObject north;
+        public GameObject east;
+        public GameObject west;
+        public GameObject south;
+    }
+
     public GameObject wall;
     public float wallLength = 1.0f;
     public int xSize = 5; // Rows X Axis
     public int ySize = 5; // Rows Y Axis
     private Vector3 initialPos;
     private GameObject wallHolder;
+
+    private Cell[] cells;
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +60,47 @@ public class Maze : MonoBehaviour
                 tempWall.name = "Wall " + i + "." + j + " (H)";
                 tempWall.transform.parent = wallHolder.transform;
             }
+        }
+
+        CreateCells();
+    }
+
+    void CreateCells()
+    {
+        int children = wallHolder.transform.childCount;
+        GameObject[] allWalls = new GameObject[children];
+        cells = new Cell[xSize * ySize];
+        int eastWestProcess = 0;
+        int childProcess = 0;
+        int termCount = 0;
+
+        // Gets all the children
+        for (int i = 0; i < children; i++)
+        {
+            allWalls[i] = wallHolder.transform.GetChild(i).gameObject;
+        }
+
+        // Assigns walls to the cells
+        for (int cellprocess = 0; cellprocess < cells.Length; cellprocess++)
+        {
+            cells[cellprocess] = new Cell();
+            cells[cellprocess].west = allWalls[eastWestProcess];
+            cells[cellprocess].south = allWalls[childProcess + (xSize + 1) * ySize];
+
+            if (termCount == xSize)
+            {
+                eastWestProcess += 2;
+                termCount = 0;
+            }
+
+            else
+                eastWestProcess++;
+
+            termCount++;
+            childProcess++;
+
+            cells[cellprocess].east = allWalls[eastWestProcess];
+            cells[cellprocess].north = allWalls[(childProcess + (xSize + 1) * ySize) + xSize - 1];
         }
     }
 
