6c73bf2470d0f6513d98118757bb4d2f7a606a33
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-01 13:39:21 +0100

    Added PlanetPuzzle to Main Game

---
+++ Assets/Scripts/Puzzles/planetsPuzle/activatePuzle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activatePuzle : MonoBehaviour
+{
+    public GameObject puzle;
+    public GameObject background;
+
+    private void OnMouseDown()
+    {
+        background.SetActive(false);
+        puzle.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (puzle.gameObject.activeSelf && GameObject.Find("PuzzleHandler").GetComponent<WinScript>().currentPoints >= GameObject.Find("PuzzleHandler").GetComponent<WinScript>().pointsToWin)
+        {
+            StartCoroutine("waitWin");
+        }
+    }
+
+    IEnumerator waitWin()
+    {
+        yield return new WaitForSeconds(1);
+        background.SetActive(true);
+        puzle.SetActive(false);
+    }
+}
