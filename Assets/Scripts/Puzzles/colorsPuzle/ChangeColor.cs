984f9b5aa042b11daf9f9cfbb1480d2a9370a2cf
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-03 00:43:01 +0100

    Colors puzzle Beta Done

---
+++ Assets/Scripts/Puzzles/colorsPuzle/ChangeColor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    private bool red, green, blue, yellow;
+
+    public bool visitado = false;
+    public SpriteRenderer[] vecinos;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (yellow)
+        {
+            yellow = false;
+            sprite.color = Color.red;
+            red = true;
+        }
+
+
+        else if (red)
+        {
+            red = false;
+            sprite.color = Color.green;
+            green = true;
+        }
+
+        else if (green)
+        {
+            green = false;
+            sprite.color = Color.blue;
+            blue = true;
+        }
+
+        else if (blue)
+        {
+            blue = false;
+            sprite.color = Color.yellow;
+            yellow = true;
+        }
+
+        else
+        {
+            sprite.color = Color.red;
+            red = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/colorsPuzle/CheckColors.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckColors : MonoBehaviour
+{
+    public GameObject fichas;
+    private Queue<GameObject> fichasQueue = new Queue<GameObject>();
+
+    public TextMesh winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DifferentColors())
+        {
+            winText.gameObject.SetActive(true);
+            fichas.SetActive(false);
+        }
+    }
+
+    public bool DifferentColors()
+    {
+        for (int i = 0; i < fichas.transform.childCount; i++)
+        {
+            fichasQueue.Enqueue(fichas.transform.GetChild(i).gameObject);
+        }
+
+        GameObject fichaActual = fichasQueue.Dequeue();
+
+        while (fichasQueue.Count != 0)
+        {
+            Color colorActual = fichaActual.GetComponent<SpriteRenderer>().color;
+            SpriteRenderer[] vecinosActuales = fichaActual.GetComponent<ChangeColor>().vecinos;
+
+            foreach (SpriteRenderer vecino in vecinosActuales)
+            {
+                if (vecino.color == colorActual || vecino.color == Color.white || colorActual == Color.white)
+                {
+                    return false;
+                }
+            }
+
+            fichaActual = fichasQueue.Dequeue();
+        }
+
+        return true;
+    }
+}
