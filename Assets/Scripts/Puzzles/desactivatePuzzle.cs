dd50f2d6dbe889787da1ae0c8725d7841395b5ba
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-10 01:05:37 +0100

    Added button to get out of the puzzles

---
+++ Assets/Scripts/Puzzles/desactivatePuzzle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desactivatePuzzle : MonoBehaviour
+{
+    [SerializeField] GameObject currentPuzzle;
+    [SerializeField] GameObject background;
+
+    private void OnMouseDown()
+    {
+        currentPuzzle.SetActive(false);
+        background.SetActive(true);
+    }
+}
