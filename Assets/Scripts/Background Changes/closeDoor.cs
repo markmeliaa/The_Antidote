113744337dbbcc58d7d59038f1cd4c6115394e45
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-11 21:20:35 +0100

    Add stuff to the first level

---
+++ Assets/Scripts/Background Changes/closeDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closeDoor : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer CorridorBackground;
+    [SerializeField] Sprite NewCorridorBackground;
+    [SerializeField] GameObject NewDoor;
+
+    private void OnMouseDown()
+    {
+        CorridorBackground.sprite = NewCorridorBackground;
+        this.gameObject.SetActive(false);
+        NewDoor.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Background Changes/openSuitcase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openSuitcase : MonoBehaviour
+{
+    [SerializeField] GameObject background;
+    [SerializeField] GameObject maletinabierto;
+    [SerializeField] GameObject miniMapIcon;
+    [SerializeField] GameObject inventoryIcon;
+
+    private void OnMouseDown()
+    {
+        background.SetActive(false);
+        maletinabierto.SetActive(true);
+        miniMapIcon.SetActive(false);
+        inventoryIcon.SetActive(false);
+    }
+}
