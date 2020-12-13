92dc0192e1e002bda4656c5930440e4783f58e81
al385729 <al385729@uji.es>
2020-12-13 19:40:44 +0100

    puzle functionality

--- Assets/Scripts/Puzzles/slidePuzle/ObjectMovement.cs
+++ Assets/Scripts/Puzzles/slidePuzle/ObjectMovement.cs
@@ -6,7 +6,8 @@ public class ObjectMovement : MonoBehaviour
 {
     Vector3 screenSpace, initialPosition, offset, startPosition;
     Sensors sensors;
-    bool horizontal;
+    public bool horizontal;
+    [SerializeField]
     bool movingRight, movingLeft, movingUp, movingDown;
 
     private void Awake()
@@ -27,17 +28,43 @@ public class ObjectMovement : MonoBehaviour
         Vector3 posicion = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
         Vector3 currentPos = Camera.main.ScreenToWorldPoint(posicion) + offset;
 
-        if(horizontal && (!sensors.blockedLeft || !sensors.blockedRight))
+        if (horizontal && (!sensors.blockedLeft || !sensors.blockedRight))
         {
             currentPos = new Vector3(currentPos.x, transform.position.y, 0);
             if (currentPos.x > initialPosition.x)
                 movingRight = true;
-            else if(currentPos.x < initialPosition.x)
+            else if (currentPos.x < initialPosition.x)
                 movingLeft = true;
         }
-        else if(!horizontal && (!sensors.blockedDown || !sensors.blockedUp))
+        else if (!horizontal && (!sensors.blockedDown || !sensors.blockedUp))
         {
             currentPos = new Vector3(transform.position.x, currentPos.y, 0);
+            if (currentPos.y > initialPosition.y)
+                movingUp = true;
+            else if (currentPos.y < initialPosition.y)
+                movingDown = true;
         }
+        else
+            return;
+
+        if (movingRight && sensors.blockedRight)
+            return;
+        else if (movingLeft && sensors.blockedLeft)
+            return;
+        else if (movingUp && sensors.blockedUp)
+            return;
+        else if (movingDown && sensors.blockedDown)
+            return;
+
+        transform.position = currentPos;
+    }
+
+    private void OnMouseUp()
+    {
+        transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0);
+        movingRight = false;
+        movingLeft = false;
+        movingUp = false;
+        movingDown = false;
     }
 }
