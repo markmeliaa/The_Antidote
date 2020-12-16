005c89eb9c2b165d2fdf349fc77724c28e1626b9
al385729 <al385729@uji.es>
2020-12-16 23:41:24 +0100

    Objectives in the inventory

---
+++ Assets/Scripts/Inventory/objectiveManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectiveManager : MonoBehaviour
+{
+    public string[] objectives;
+    public GameObject[] locations;
+    public int[] timesVisited;
+    public sceneManager manager;
+
+    int index = 0;
+
+    void Update()
+    {
+        if(index < objectives.Length && manager.getLocationBool(locations[index].name) 
+            && manager.getLocationTimes(locations[index].name) == timesVisited[index])
+        {
+            transform.GetChild(0).GetComponent<Text>().text = objectives[index];
+            index++;
+        }
+    }
+}
