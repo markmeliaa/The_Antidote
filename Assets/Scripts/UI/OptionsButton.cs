3b9f413687eb701158c51f2c5504e446f508fc32
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-17 23:04:48 +0100

    Exit button working

---
+++ Assets/Scripts/UI/OptionsButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsButton : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+    [SerializeField] GameObject circleWipe;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (menuButtonController.index == thisIndex)
+        {
+            animator.SetBool("selected", true);
+            // Pressed
+            if (Input.GetAxis("Submit") == 1)
+            {
+                animator.SetBool("pressed", true);
+                circleWipe.SetActive(true);
+                circleWipe.GetComponent<Animator>().SetTrigger("Start");
+                circleWipe.GetComponent<Animator>().SetTrigger("Start");
+            }
+
+            else if (animator.GetBool("pressed"))
+            {
+                animator.SetBool("pressed", false);
+                animatorFunctions.disableOnce = true;
+            }
+        }
+
+        else
+            animator.SetBool("selected", false);
+    }
+}
