a9640cf6b2f75f6aac1626cd230717c9da7cc903
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-18 20:41:19 +0100

    Some city sound effects

---
+++ Assets/Scripts/Background Changes/moveBoxes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBoxes : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer BoxesBackground;
+    [SerializeField] Sprite NewBoxesBackground;
+    [SerializeField] GameObject newBoxes;
+    private AudioSource boxesSound;
+    [SerializeField] GameObject arrow;
+
+    private void Start()
+    {
+        boxesSound = GetComponent<AudioSource>();
+    }
+
+    private void OnMouseDown()
+    {
+        BoxesBackground.sprite = NewBoxesBackground;
+        boxesSound.Play();
+        this.gameObject.GetComponent<Collider2D>().enabled = false;
+        arrow.SetActive(true);
+        newBoxes.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UI/AnimatorMap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorMap : MonoBehaviour
+{
+    [SerializeField] GameObject gameobject;
+
+    void PlaySound(AudioClip whichSound)
+    {
+        gameobject.GetComponent<AudioSource>().PlayOneShot(whichSound);
+    }
+}
