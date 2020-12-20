02f956e8b6ef88c2bbf79b5f13da886e0406e830
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-20 21:29:26 +0100

    Pickup glowing items city done

---
+++ Assets/Scripts/Inventory/glowingItems.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glowingItems : MonoBehaviour
+{
+    [SerializeField] Sprite glowingItem;
+    private SpriteRenderer item;
+    private Sprite normalItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        item = GetComponent<SpriteRenderer>();
+        normalItem = item.sprite;
+    }
+
+    private void OnMouseEnter()
+    {
+        item.sprite = glowingItem;
+    }
+
+    private void OnMouseExit()
+    {
+        item.sprite = normalItem;
+    }
+
+    private void OnBecameVisible()
+    {
+        item.sprite = normalItem;
+    }
+}
