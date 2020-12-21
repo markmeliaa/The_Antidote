d2183a5632f01ee2e6d9ca1565e674c5d6017750
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-21 01:39:14 +0100

    A lot of things done

---
+++ Assets/Scripts/Sounds/doorSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorSound : MonoBehaviour
+{
+    private AudioSource sound;
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void OnMouseDown()
+    {
+        sound.Play();
+    }
+}
