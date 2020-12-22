f7ad0a1d636bd21a13e1d3dae189d8beeaf1d1e6
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-22 13:08:58 +0100

    Credits end scene

---
+++ Assets/Scripts/UI/ToBeContinued.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToBeContinued : MonoBehaviour
+{
+    [SerializeField] GameObject circleWipe;
+    [SerializeField] GameObject currentcircleWipe;
+    [SerializeField] Canvas currentCanvas;
+    [SerializeField] Canvas nextCanvas;
+
+    private void Start()
+    {
+        StartCoroutine("changeScreen");
+    }
+
+    IEnumerator changeScreen()
+    {
+        yield return new WaitForSeconds(3);
+        circleWipe.SetActive(true);
+        circleWipe.GetComponent<Animator>().SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        circleWipe.SetActive(false);
+        currentcircleWipe.SetActive(true);
+        currentCanvas.gameObject.SetActive(false);
+        nextCanvas.gameObject.SetActive(true);
+    }
+}
