6e3ba54c6e80296c0c949fe2270f58d72fdd1318
al385729 <al385729@uji.es>
2020-12-29 12:31:51 +0100

    bus scene

---
+++ Assets/Scripts/Navigation/busDoorActivation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class busDoorActivation : MonoBehaviour
+{
+    public GameObject nextBusDoor;
+    public GameObject currentBusDoor;
+    public GameObject altBusDoor;
+
+    private bool active = true;
+    private void Update()
+    {
+        if (altBusDoor.activeSelf)
+            active = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (active)
+        {
+            currentBusDoor.SetActive(false);
+            nextBusDoor.SetActive(true);
+        }
+    }
+}
