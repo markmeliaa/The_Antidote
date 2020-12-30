cdd4532920727d6e7809095d6daad54507e4b4c1
Mark Meliaa <marcalconmelia@gmail.com>
2020-12-30 11:51:34 +0100

    Chemistry and picross fixed

--- Assets/Scripts/Puzzles/picrossPuzle/CountPoints.cs
+++ Assets/Scripts/Puzzles/picrossPuzle/CountPoints.cs
@@ -8,12 +8,18 @@ public class CountPoints : MonoBehaviour
     [SerializeField] GameObject casillasBlancas;
     [SerializeField] GameObject casillasNegras;
     private int totalPoints = 0;
+    private int blancoPoints = 0;
     [SerializeField] GameObject winText;
+
+    private void Start()
+    {
+        blancoPoints = casillasBlancas.transform.childCount;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (totalPoints >= casillasNegras.transform.childCount)
+        if (totalPoints == casillasNegras.transform.childCount && blancoPoints == casillasBlancas.transform.childCount - casillasNegras.transform.childCount)
         {
             for (int i = 0; i < casillasBlancas.transform.childCount; i++)
             {
@@ -29,12 +35,26 @@ public class CountPoints : MonoBehaviour
             if (casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().marcada && casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().averiguada && !casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada)
             {
                 totalPoints++;
+                blancoPoints--;
                 casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada = true;
             }
 
             else if (casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().marcada && !casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().averiguada && casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada)
             {
                 totalPoints--;
+                blancoPoints++;
+                casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada = false;
+            }
+
+            else if (!casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().marcada && casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().averiguada && !casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada)
+            {
+                blancoPoints--;
+                casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada = true;
+            }
+
+            else if (!casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().marcada && !casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().averiguada && casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada)
+            {
+                blancoPoints++;
                 casillasBlancas.transform.GetChild(i).GetComponent<ChangeTile>().destapada = false;
             }
         }
