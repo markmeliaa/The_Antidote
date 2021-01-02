dde6f5dd1d68925142b7da014c00926cde89d426
al385729 <al385729@uji.es>
2021-01-02 22:55:20 +0100

    city map controlled

---
+++ Assets/Scripts/Map/MapDesactivation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDesactivation : MonoBehaviour
+{
+    public GameObject[] ubications;
+    public GameObject[] conditions;
+    public int[] timesVisited;
+
+
+    sceneManager manager;
+    int index;
+
+    private void Start()
+    {
+        manager = GetComponentInParent<sceneManager>();
+    }
+    private void Update()
+    {
+        if (index < timesVisited.Length && manager.getLocationTimes(conditions[index].name) == timesVisited[index])
+        {
+            checkDesactivation();
+        }
+    }
+
+    private void checkDesactivation()
+    {
+        ubications[index].GetComponent<CircleCollider2D>().enabled = false;
+        ubications[index].GetComponent<CursorObject>().active = false;
+        index++;
+    }
+}
