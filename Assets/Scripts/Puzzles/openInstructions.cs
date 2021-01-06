6d08f60bd3e847418495c03393d8bd38033ff18a
Mark Meliaa <marcalconmelia@gmail.com>
2021-01-06 16:05:48 +0100

    Pipe Puzzle Added

---
+++ Assets/Scripts/Puzzles/openInstructions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openInstructions : MonoBehaviour
+{
+    private Animator instructAnimator;
+    [SerializeField] GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructAnimator = GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (instructAnimator.GetBool("Show"))
+        {
+            instructAnimator.SetBool("Show", false);
+            text.SetActive(false);
+        }
+
+        else if (!instructAnimator.GetBool("Show"))
+        {
+            instructAnimator.SetBool("Show", true);
+            StartCoroutine("showText");
+        }
+    }
+
+    IEnumerator showText()
+    {
+        yield return new WaitForSeconds(0.45f);
+        text.SetActive(true);
+    }
+}
