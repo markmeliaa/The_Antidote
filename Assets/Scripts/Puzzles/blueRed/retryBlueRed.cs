badc8aa1f0d93a6f0d576261f6f77749c34489e4
al385729 <al385729@uji.es>
2021-01-07 14:50:47 +0100

    blueRed puzle functionality

---
+++ Assets/Scripts/Puzzles/blueRed/retryBlueRed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class retryBlueRed : MonoBehaviour
+{
+    public GameObject gameArea;
+    public GameObject winText;
+    private void OnMouseDown()
+    {
+        if(!winText.activeSelf)
+            gameArea.GetComponent<puzleController>().retryPuzle();
+    }
+}
