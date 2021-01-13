84bf225c3d5bc71c526d1cbb2d5f3b8b868939fa
al385729 <al385729@uji.es>
2021-01-13 17:50:32 +0100

    map controlled & map changing

---
+++ Assets/Scripts/Map/changeMiniMap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeMiniMap : MonoBehaviour
+{
+    private GameObject currentMap;
+    private GameObject nextMap;
+    bool changed = false;
+
+    public sceneManager manager;
+    public GameObject location;
+    public int timesVisited;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMap = transform.Find("EnterpriseMap").gameObject;
+        nextMap = transform.Find("EnterpriseMap").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!changed && manager.getLocationTimes(location.name) == timesVisited)
+        {
+            currentMap.SetActive(false);
+            nextMap.SetActive(true);
+            changed = true;
+        }
+    }
+}
