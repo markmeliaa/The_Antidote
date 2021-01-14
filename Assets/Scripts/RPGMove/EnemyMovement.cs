a7a6272787c1b514c83498d11b480f73ab78cef4
Mark Meliaa <marcalconmelia@gmail.com>
2021-01-14 13:23:20 +0100

    Add npcs automatic movement

---
+++ Assets/Scripts/RPGMove/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private Transform playerTransform;
+    private PlayerController playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = player.GetComponent<Transform>();
+        playerScript = player.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (this.transform.position.y <= playerTransform.position.y)
+        {
+            playerScript.canMove = false;
+
+            if (transform.position.x <= playerTransform.position.x - 0.75)
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, Time.deltaTime);
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Debug.Log("ahora");
+    }
+}
--- Assets/Scripts/RPGMove/PlayerController.cs
+++ Assets/Scripts/RPGMove/PlayerController.cs
@@ -7,22 +7,30 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+    public bool canMove;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!canMove)
+            return;
+
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
     }
 
     void FixedUpdate()
     {
+        if (!canMove)
+            return;
+
         Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.5f * horizontal * Time.deltaTime;
         position.y = position.y + 3.5f * vertical * Time.deltaTime;
