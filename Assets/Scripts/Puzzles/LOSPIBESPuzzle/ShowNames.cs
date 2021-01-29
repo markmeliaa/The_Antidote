44e881b3ed5be8bafeae4013843d7fbd8dda5be2
Mark Meliaa <marcalconmelia@gmail.com>
2021-01-29 21:16:34 +0100

    LOS PIBES added

---
+++ Assets/Scripts/Puzzles/LOSPIBESPuzzle/ShowNames.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowNames : MonoBehaviour
+{
+    [SerializeField] TextMesh currentName;
+
+    private void OnMouseOver()
+    {
+        currentName.gameObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        currentName.gameObject.SetActive(false);
+    }
+}
